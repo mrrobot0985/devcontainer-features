a45d75829696582fda421c0bb7619b63d0b9f70f
Mister Robot <164061086+mrrobot0985@users.noreply.github.com>
2026-07-16 14:55:01 +0200

    fix: direnv-integration install via apt-get only (#60)

--- test/direnv-integration/default.sh
+++ test/direnv-integration/default.sh
@@ -3,8 +3,8 @@ set -e
 
 source dev-container-features-test-lib
 
-check "direnv binary exists" test -x /usr/local/bin/direnv
-check "direnv version works" bash -c "direnv version | grep -qE '^[0-9]+\.'"
+check "direnv binary exists" bash -c "command -v direnv >/dev/null 2>&1"
+check "direnv version works" bash -c "direnv version >/dev/null 2>&1"
 check "direnvrc exists" test -f /home/vscode/.direnvrc
 
 reportResults
