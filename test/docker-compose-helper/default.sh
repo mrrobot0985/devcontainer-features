53d1bab1ffeafd4da9a37e99ade3fc910a44e64e
Mister Robot <164061086+mrrobot0985@users.noreply.github.com>
2026-07-16 15:56:51 +0000

    test: fix docker-compose-helper test assertions to match actual install paths

--- test/docker-compose-helper/default.sh
+++ test/docker-compose-helper/default.sh
@@ -4,6 +4,6 @@ set -e
 source dev-container-features-test-lib
 
 check "cli exists" command -v devcontainer-compose-check
-check "helper script exists" test -x /usr/local/share/docker-compose-helper/compose-helper.sh
+check "cli is executable" test -x /usr/local/bin/devcontainer-compose-check
 
 reportResults
--- test/docker-compose-helper/test.sh
+++ test/docker-compose-helper/test.sh
@@ -4,7 +4,6 @@ set -e
 source dev-container-features-test-lib
 
 check "cli exists" command -v devcontainer-compose-check
-check "install script exists" test -x /usr/local/share/docker-compose-helper/compose-helper.sh
-check "helper script exists" test -x /usr/local/share/docker-compose-helper/compose-helper.sh
+check "cli is executable" test -x /usr/local/bin/devcontainer-compose-check
 
 reportResults
