65499ff9043bd70a488e8332fa5b8799b5da9848
Mister Robot <164061086+mrrobot0985@users.noreply.github.com>
2026-07-16 12:45:16 +0200

    fix: taskfile-dev version test regex to match actual output format (#52)

--- test/taskfile-dev/test.sh
+++ test/taskfile-dev/test.sh
@@ -4,6 +4,6 @@ set -e
 source dev-container-features-test-lib
 
 check "task binary exists" test -x /usr/local/bin/task
-check "task version works" bash -c "task --version | grep -qE '^task\s+version' || task --version | grep -qE 'v[0-9]+\.'"
+check "task version works" bash -c "task --version | grep -qE '^[0-9]+\.[0-9]+'"
 
 reportResults
